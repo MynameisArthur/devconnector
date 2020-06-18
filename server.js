b0f9a36da026ebb73ffc8284c164677847b6ffdc
Arthur <arthurd800@wp.pl>
2020-06-18 15:31:13 +0200

    added folders:config, models,routes; added 4 routes, connected to mongoDB, added config file

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const connectDB = require('./config/db');
+
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+//connect DB
+connectDB();
+
+app.get('/', (req, res) => {
+    res.send('&lt;MERN&gt; API running...');
+});
+//Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+
+app.listen(PORT, (req, res) => {
+    console.log(`Server started on port: ${PORT}`);
+});
