001889f6fe6c2d27e4362374008e92d5548ae139
Arthur <arthurd800@wp.pl>
2020-06-25 13:22:21 +0200

    added Alert component, added redux, connected register component with redux

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,30 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+//Components
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Login from './components/auth/Login';
+import Alert from './components/layout/Alert';
+import Register from './components/auth/Register';
+//Redux
+import { Provider } from 'react-redux';
+import store from './store';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Devconnector</h1>
-    </div>
-  );
-}
+const App = () => (
+  <Provider store={store}>
+    <Router>
+      <Navbar />
+      <Route exact path="/" component={Landing} />
+      <section className="container">
+        <Alert />
+        <Switch>
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/register" component={Register} />
+        </Switch>
+      </section>
+    </Router>
+  </Provider>
+);
 
 export default App;
